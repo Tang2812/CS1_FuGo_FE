05612dbd968c7e6e4b3737781372b9de6317a4c3
PhanAnhHao <pdkenny25@gmail.com>
2024-10-29 17:33:44 +0700

    Convert SCSS to TailwindCSS

--- CS1_FuGo_FE/src/pages/Home_components/HomePagePartners.jsx
+++ CS1_FuGo_FE/src/pages/Home_components/HomePagePartners.jsx
@@ -3,7 +3,7 @@ import partnerImg from "../../img/anh_cong_viec.png";
 const HomePagePartners = () => {
     return (
         <div className="homePagePartners-container mx-6 pb-4 pt-2">
-            <h1 className="font-medium text-lg ml-16">Top đối tác Du học - XKLĐ</h1>
+            <h1 className="text-lg font-semibold ml-16">Top đối tác Du học - XKLĐ</h1>
             <div className="partner-content grid grid-cols-6">
                 <div className="partner-tag">
                     <a href="#">
--- CS1_FuGo_FE/src/pages/Home_components/HomeSuggest.jsx
+++ CS1_FuGo_FE/src/pages/Home_components/HomeSuggest.jsx
@@ -3,15 +3,14 @@ import suggestTag from "../../img/tagImg.jpg";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { MdPlace } from "react-icons/md";
 import { FaClock } from "react-icons/fa";
-import "../../stylesheet/HomeSuggest.scss";
 
 const HomeSuggest = () => {
     return (
-        <div className="homeSuggest-container">
-            <div className="suggest-header">
+        <div className="homeSuggest-container bg-[#F6F6F6] w-full pt-5 pr-[60px] pb-5 pl-[90px]">
+            <div className="suggest-header flex justify-between mb-5">
                 <div className="suggest-title-container flex gap-2">
-                    <FaMedal className="suggest-icon" />
-                    <h1 className="suggest-title">
+                    <FaMedal className="suggest-icon text-[#dbb313] text-[24px]" />
+                    <h1 className="suggest-title text-xl font-semibold">
                         Đề xuất
                     </h1>
                 </div>
@@ -19,98 +18,96 @@ const HomeSuggest = () => {
                     <a href="" className="text-yellow-500">Xem tất cả &gt;</a>
                 </div>
             </div>
-            <div className="suggest-categories space-x-4">
-                <a href="#" className="category-item">
+            <div className="suggest-categories space-x-4 mb-[30px]">
+                <a href="#" className="category-item border-2 border-solid border-black rounded-[5px] px-4 py-2">
                     Tất cả
                 </a>
-                <a href="#" className="category-item">
+                <a href="#" className="category-item border-2 border-solid border-black rounded-[5px] px-4 py-2">
                     Nhật Bản
                 </a>
-                <a href="#" className="category-item">
+                <a href="#" className="category-item border-2 border-solid border-black rounded-[5px] px-4 py-2">
                     Hàn Quốc
                 </a>
-                <a href="#" className="category-item">
+                <a href="#" className="category-item border-2 border-solid border-black rounded-[5px] px-4 py-2">
                     Đài Loan
                 </a>
             </div>
             <div className="suggest-content grid grid-cols-4">
-                <a href="#" className="suggest-tag">
-                    <img src={suggestTag} alt="" className="tag-img" />
-                    <div className="tag-content">
+                <a href="#" className="suggest-tag bg-white shadow-2xl rounded-[10px] w-4/5 mb-5 hover:mt-[-4px]">
+                    <img src={suggestTag} alt="" className="tag-img rounded-tl-[10px] rounded-tr-[10px] w-fit" />
+                    <div className="tag-content p-2">
                         <p className="text-lg font-medium">Vệ sinh tòa nhà</p>
                         <div className="flex gap-2 items-center">
-                            <FaMoneyBillWave className="suggest-salary-icon" />
+                            <FaMoneyBillWave className="suggest-salary-icon text-green-500" />
                             <p>100,000,000</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <MdPlace className="suggest-place-icon" />
+                            <MdPlace className="suggest-place-icon text-red-600" />
                             <p>Aichi</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <FaClock className="suggest-due-icon" />
+                            <FaClock className="suggest-due-icon  text-blue-700" />
                             <p>14/10/2024</p>
                         </div>
                     </div>
                 </a>
 
-                <a href="#" className="suggest-tag">
-                    <img src={suggestTag} alt="" className="tag-img" />
-                    <div className="tag-content">
+                <a href="#" className="suggest-tag bg-white shadow-2xl rounded-[10px] w-4/5 mb-5 hover:mt-[-4px]">
+                    <img src={suggestTag} alt="" className="tag-img rounded-tl-[10px] rounded-tr-[10px] w-fit" />
+                    <div className="tag-content p-2">
                         <p className="text-lg font-medium">Vệ sinh tòa nhà</p>
                         <div className="flex gap-2 items-center">
-                            <FaMoneyBillWave className="suggest-salary-icon" />
+                            <FaMoneyBillWave className="suggest-salary-icon text-green-500" />
                             <p>100,000,000</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <MdPlace className="suggest-place-icon" />
+                            <MdPlace className="suggest-place-icon text-red-600" />
                             <p>Aichi</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <FaClock className="suggest-due-icon" />
+                            <FaClock className="suggest-due-icon  text-blue-700" />
                             <p>14/10/2024</p>
                         </div>
                     </div>
                 </a>
 
-                <a href="#" className="suggest-tag">
-                    <img src={suggestTag} alt="" className="tag-img" />
-                    <div className="tag-content">
+                <a href="#" className="suggest-tag bg-white shadow-2xl rounded-[10px] w-4/5 mb-5 hover:mt-[-4px]">
+                    <img src={suggestTag} alt="" className="tag-img rounded-tl-[10px] rounded-tr-[10px] w-fit" />
+                    <div className="tag-content p-2">
                         <p className="text-lg font-medium">Vệ sinh tòa nhà</p>
                         <div className="flex gap-2 items-center">
-                            <FaMoneyBillWave className="suggest-salary-icon" />
+                            <FaMoneyBillWave className="suggest-salary-icon text-green-500" />
                             <p>100,000,000</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <MdPlace className="suggest-place-icon" />
+                            <MdPlace className="suggest-place-icon text-red-600" />
                             <p>Aichi</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <FaClock className="suggest-due-icon" />
+                            <FaClock className="suggest-due-icon  text-blue-700" />
                             <p>14/10/2024</p>
                         </div>
                     </div>
                 </a>
 
-                <a href="#" className="suggest-tag">
-                    <img src={suggestTag} alt="" className="tag-img" />
-                    <div className="tag-content">
+                <a href="#" className="suggest-tag bg-white shadow-2xl rounded-[10px] w-4/5 mb-5 hover:mt-[-4px]">
+                    <img src={suggestTag} alt="" className="tag-img rounded-tl-[10px] rounded-tr-[10px] w-fit" />
+                    <div className="tag-content p-2">
                         <p className="text-lg font-medium">Vệ sinh tòa nhà</p>
                         <div className="flex gap-2 items-center">
-                            <FaMoneyBillWave className="suggest-salary-icon" />
+                            <FaMoneyBillWave className="suggest-salary-icon text-green-500" />
                             <p>100,000,000</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <MdPlace className="suggest-place-icon" />
+                            <MdPlace className="suggest-place-icon text-red-600" />
                             <p>Aichi</p>
                         </div>
                         <div className="flex gap-2 items-center">
-                            <FaClock className="suggest-due-icon" />
+                            <FaClock className="suggest-due-icon  text-blue-700" />
                             <p>14/10/2024</p>
                         </div>
                     </div>
                 </a>
-
-
             </div>
         </div>
     );
