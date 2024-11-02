3f6f2c929f801642fc4fd88735def5fd96ff8fca
DvHuy <ahuyahihi4@gmail.com>
2024-11-02 15:08:29 +0700

    feat: PartnerDashboard

--- CS1_FuGo_FE/src/components/JobCV/ViewListJobCV.jsx
+++ CS1_FuGo_FE/src/components/JobCV/ViewListJobCV.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import DataTable from "react-data-table-component";
 const ViewListJobCV = () => {
   return (
-    <div>ViewListJobCV</div>
+    <div className="p-5">
+    <div className="text-center">
+      <h3 className="text-2xl font-bold">Manage Jobs</h3>
+    </div>
+    <div className="flex justify-between items-center">
+      <input
+        type="text"
+        placeholder="Search By Name"
+        className="px-4 py-0.5 rounded border"
+       
+      />
+      <Link
+        to="/admin-dashboard/add-department"
+        className="px-4 py-1 bg-teal-600 rounded text-white"
+      >
+        Add New Job
+      </Link>
+    </div>
+    <div className="mt-5">
+      <DataTable  pagination />
+    </div>
+  </div>
   )
 }
 
--- CS1_FuGo_FE/src/layout/Layout.jsx
+++ CS1_FuGo_FE/src/layout/Layout.jsx
@@ -1,16 +1,11 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Routers from "../router/Routers";
-import PartnerDashboard from "../pages/PartnerDashboard";
-import ViewListJobCV from "../components/JobCV/ViewListJobCV";
-import ViewListSudyAbroadCV from "../components/StudyCV/ViewListSudyAbroadCV";
-import CreateJob from "../components/CreateJob/CreateJob";
-import CreateStudyAbroad from "../components/CreateStudyAbroad/CreateStudyAbroad";
 import PartnerRouter from "../router/PartnerRouter";
 
 
 const Layout = () => {
-  const user = "user";
+  const user = "partner";
   return (
     <>
       {user === "user" ? (
