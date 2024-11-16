27539275f6c4d8af859c5331ae9c153d4885d144
Tang2812 <dinhvietnhattan@gmail.com>
2024-11-16 16:03:23 +0700

    fix: fix toast error login logout partner

--- CS1_FuGo_FE/src/components/Dashboard/Navbar.jsx
+++ CS1_FuGo_FE/src/components/Dashboard/Navbar.jsx
@@ -1,12 +1,25 @@
-
-
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
-   
+    const [auth, setAuth] = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        toast.success("Logout Successfully");
+        setAuth({
+            ...auth,
+            user: null,
+            token: "",
+        });
+        localStorage.removeItem("auth");
+        navigate("/login");
+    };
   return (
     <div className="flex items-center text-white justify-between h-12 bg-teal-600 px-5">
         <p>Welcom, `Tên partner` </p>
-        <button className="px-4 py-1 bg-teal-700 hover:bg-teal-800 rounded">Logout</button>
+        <button className="px-4 py-1 bg-teal-700 hover:bg-teal-800 rounded" onClick={handleLogout}>Logout</button>
     </div>
   )
 }
