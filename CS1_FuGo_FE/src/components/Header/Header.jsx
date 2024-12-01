3f5b8dde8d9abf0029a2c92c322facd3f48fa463
phuctien1909 <nguyenhoangphuctien190903@gmail.com>
2024-12-02 00:57:02 +0700

    update user-profile

--- CS1_FuGo_FE/src/components/Header/Header.jsx
+++ CS1_FuGo_FE/src/components/Header/Header.jsx
@@ -153,7 +153,7 @@ const Header = () => {
                     </Link>
                     <Link to="#" className="property__choice">
                       <AiOutlineAudit />
-                      <span>Chỉnh sửa trang các nhân</span>
+                      <span>Chỉnh sửa trang cá nhân</span>
                     </Link>
                   </div>
                   <div className="property-line" />
